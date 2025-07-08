88d11b843115cf347fbde2e15185fe6ff32cbec0
Muliro1 <mulirokhaemba@gmail.com>
2025-07-08 21:56:45 +0300

    utils

--- properties/views.py
+++ properties/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.views.decorators.cache import cache_page
+from .utils import get_all_properties
 
 # Create your views here.
+
+@cache_page(60 * 15)
+def property_list(request):
+    properties = get_all_properties()
+    return render(request, 'properties/property_list.html', {'properties': properties})
